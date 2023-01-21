e41885c38fb2697feec74181a7be62f16d2da6d7
RostyslavMyag <myagkovrostyslav1@gmail.com>
2023-01-21 22:25:39 +0200

    First commit, choosing libs

---
+++ SSTC_Interrupter/main/square_wave.c
@@ -0,0 +1,10 @@
+/*
+ * square_wave.c
+ *
+ *  Created on: 19 Jan 2023
+ *      Author: Rostyslav
+ */
+
+
+
+
---
+++ SSTC_Interrupter/main/square_wave.h
@@ -0,0 +1,15 @@
+/*
+ * square_wave.h
+ *
+ *  Created on: 19 Jan 2023
+ *      Author: Rostyslav
+ */
+
+#ifndef MAIN_SQUARE_WAVE_H_
+#define MAIN_SQUARE_WAVE_H_
+
+// Define main variables for
+
+
+
+#endif /* MAIN_SQUARE_WAVE_H_ */
